5a941046b557df87205ef2ddf4b7f175eb108c9b
JdenHartog <J.den.Hartog@vu.nl>
2015-08-11 15:37:27 +0200

    Connect to VU-AMS only in the first prepare
    Several parts of the preparations only have to happen ones (i.e. not for
    every vu_ams item in the exp):
    1) Loading the dll
    2) Connecting to the device
    3) Checking if device is recording
    4) Appending Close to cleanup_functions
    Because the item.py get and set functions can only handle simple
    variable types (unicode, float, int);
    we can not store the <class 'ctypes.WinDLL'> returned by
    windll.amsserial globally.
    Therefore we load the dll (1) in every prepare. Luckily it seems to
    return the same handle every time.
    To track if the device is already connected (so 2, 3 and 4 can be
    skipped) we use the global variable
    vuamsconnected.

--- vu_ams/vu_ams.py
+++ vu_ams/vu_ams.py
@@ -58,6 +58,18 @@ class vu_ams(item):
 		# Call the parent constructor.
 		item.prepare(self)
 
+		# Several parts of the preparations only have to happen ones (i.e. not for every vu_ams item in the exp):
+		# 1) Loading the dll 
+		# 2) Connecting to the device
+		# 3) Checking if device is recording
+		# 4) Appending Close to cleanup_functions
+		# Because the item.py get and set functions can only handle simple variable types (unicode, float, int);
+		# we can not store the <class 'ctypes.WinDLL'> returned by windll.amsserial globally.
+		# Therefore we load the dll (1) in every prepare. Luckily it seems to return the same handle every time. 
+		# To track if the device is already connected (so 2, 3 and 4 can be skipped) we use the global variable
+		# vuamsconnected.
+
+		
 		# Load dll to communicate with VU-AMS device
 		try:
 			from ctypes import windll
@@ -67,55 +79,71 @@ class vu_ams(item):
 			raise osexception( \
 				u'AmsSerial.dll not found. Download (and install) from www.vu-ams.nl >  support  >  downloads  >  extra  >  Download AMS serial DLL setup version 1.3.5 ')
 		
-		# If a device has been specified, use it
-		if self._device_name not in (None, '', u'autodetect'):
-			self._vuams = self._device_name
-			try:
-				self.AMS.Connect(str(self._vuams), 'AMS5fs') #NOTE: device type can't be u'unicode'
-				debug.msg(u'Trying to connect to VU-AMS device')
-			except Exception as e:
-				raise osexception( u'Failed to open device port "%s" : "%s"' % (self._vuams, e))
+		try:
+			self.experiment.get(u'vuamsconnected')
+		except:
+			#######################
+			# Only ones:
+			#######################
+			
+			# If a device has been specified, use it
+			if self._device_name not in (None, '', u'autodetect'):
+				self._vuams = self._device_name
+				try:
+					self.AMS.Connect(str(self._vuams), 'AMS5fs') #NOTE: device type can't be u'unicode'
+					debug.msg(u'Trying to connect to VU-AMS device')
+				except Exception as e:
+					raise osexception( u'Failed to open device port "%s" : "%s"' % (self._vuams, e))
 
-			# Try to get VU-AMS Serial to check is a VU-AMS device is connected
-			if(self.AMS.GetSerial()<=0):
-				raise osexception( u'Failed to connect to device on port "%s"' % (self._vuams))
+				# Try to get VU-AMS Serial to check is a VU-AMS device is connected
+				if(self.AMS.GetSerial()<=0):
+					raise osexception( u'Failed to connect to device on port "%s"' % (self._vuams))
 
-		else:
-			# Else determine the common name of the serial devices on the
-			# platform and find the first accessible device. On Windows,
-			# devices are labeled COM[X], on Linux there are labeled /dev/tty[X]
-			debug.msg(u'Trying to connect to VU-AMS device using autodetect')
-			if os.name == u'nt':
-				for i in range(255):
-					try:
-						dev = u'COM%d' % (i+1) #as COM ports start from 1 on Windows
-						self.AMS.Connect(str(dev), 'AMS5fs') #NOTE: device type can't be u'unicode'
-						# Try to get VU-AMS Serial to check is a VU-AMS device is connected
-						if(self.AMS.GetSerial()>0):
-							self._vuams = dev
-							break
-						self.AMS.Disconnect()
-					except Exception as e:
-						self._vuams = None
-						pass
-
-			elif os.name == u'posix':
-				raise osexception( \
-					u'Sorry: the vu-ams plug-in is Windows only.')
 			else:
-				raise osexception( \
-					u'vu-ams plug-in does not know how to auto-detect the VU-AMS on your platform. Please specify a device.')
+				# Else determine the common name of the serial devices on the
+				# platform and find the first accessible device. On Windows,
+				# devices are labeled COM[X], on Linux there are labeled /dev/tty[X]
+				debug.msg(u'Trying to connect to VU-AMS device using autodetect')
+				if os.name == u'nt':
+					for i in range(255):
+						try:
+							dev = u'COM%d' % (i+1) #as COM ports start from 1 on Windows
+							self.AMS.Connect(str(dev), 'AMS5fs') #NOTE: device type can't be u'unicode'
+							# Try to get VU-AMS Serial to check is a VU-AMS device is connected
+							if(self.AMS.GetSerial()>0):
+								self._vuams = dev
+								break
+							self.AMS.Disconnect()
+						except Exception as e:
+							self._vuams = None
+							pass
 
-		if self._vuams == None:
-			raise osexception( \
-				u'vu-ams plug-in failed to auto-detect a VU-AMS. Please specify a device.')
-		else:
-			print u'Connected to VU-AMS'
+				elif os.name == u'posix':
+					raise osexception( \
+						u'Sorry: the vu-ams plug-in is Windows only.')
+				else:
+					raise osexception( \
+						u'vu-ams plug-in does not know how to auto-detect the VU-AMS on your platform. Please specify a device.')
+
+			if self._vuams == None:
+				raise osexception( \
+					u'vu-ams plug-in failed to auto-detect a VU-AMS. Please specify a device.')
+			else:
+				self.experiment.set(u'vuamsconnected',u'yes')
+				print u'Connected to VU-AMS'
 		
-		# Check if VU-AMS device is recording
-		if(self.AMS.IsRecording()!=1):
-			raise osexception(u'VU-AMS is not recording!')
-		
+
+			# Check if VU-AMS device is recording
+			if(self.AMS.IsRecording()!=1):
+				raise osexception(u'VU-AMS is not recording!')
+			
+			self.experiment.cleanup_functions.append(self.close)
+
+			#######################
+			# END: Only ones	###
+			#######################
+
+
 		# Check if marker is numerical
 		try:
 			int(self.get(u'_send_marker'))
@@ -125,9 +153,6 @@ class vu_ams(item):
 		if(self.get(u'_send_marker')>65535):
 			raise osexception(u'Markers can not be bigger then 65535')
 	
-		self.experiment.cleanup_functions.append(self.close)
-
-
 		
 	def run(self):
 
